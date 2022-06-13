3323ad63a897d49c0cd8488ff7d85f8732051088
oxisakre <leoalvarg@gmail.com>
2022-06-13 21:01:26 +0200

    ejercicio

--- book/ejercicio 14.1.py
+++ book/ejercicio 14.1.py
@@ -1,25 +1,44 @@
 class Hotel:
- """Representa un hotel: sus atributos son:
- nombre, ubicacion, puntaje y precio."""
+    """Representa un hotel: sus atributos son:
+        nombre, ubicacion, puntaje y precio."""
 
-def __init__(self, nombre, ubicacion, puntaje, precio):
+
+    def __init__(self, nombre, ubicacion, puntaje, precio):
         """Crea un Hotel.
         nombre y ubicacion deben ser cadenas no vacías,
         puntaje y precio son números."""
-        self.nombre = validar_cadena_no_vacia(nombre)
-        self.ubicacion = validar_cadena_no_vacia(ubicacion)
-        self.puntaje = validar_numero_positivo(puntaje)
-        self.precio = validar_numero_positivo(precio)
+        self.nombre = nombre
+        self.ubicacion = ubicacion
+        self.puntaje = puntaje
+        self.precio = precio
 
-def __str__(self):
+
+    def __str__(self):
         """Conversión a cadena de texto."""
         return "{} de {} - Puntaje: {} - Precio: {} pesos".format(
-        self.nombre,
-        self.ubicacion,
-        self.puntaje,
-        self.precio,
+            self.nombre,
+            self.ubicacion,
+            self.puntaje,
+            self.precio,
         )
 
-def ratio(self):
+
+    def __lt__(self, hotel):
+        ubi = self.ubicacion < hotel.ubicacion
+        if self.ubicacion != hotel.ubicacion:
+            return ubi
+
+        return self.ratio() < hotel.ratio()  
+        
+
+
+    def ratio(self):
         """Calcula la relación calidad-precio de un hotel"""
         return ((self.puntaje ** 2) * 10) / self.precio
+
+h = Hotel("Hotel City", "Mercedes", 3.25, 78)
+i = Hotel("Hotel Mascardi", "Mercedes", 6, 150)
+j = [i, h]
+j.sort()
+print(str(j[0])),print('---') , print(str(j[1]))
+print(i.ratio(), h.ratio())
