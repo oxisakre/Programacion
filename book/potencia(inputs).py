518d6cdaa9ed34872960718d38cfca30928a0539
oxisakre <leoalvarg@gmail.com>
2022-06-25 18:37:31 +0200

    ejercicios

--- book/potencia(inputs).py
+++ book/potencia(inputs).py
@@ -8,7 +8,7 @@ def is_power_of(number, base):
   if number < base:
     # If number is equal to 1, it's a power (base**0).
     return number == 1
-  result = number//base
+  result = number/base
   # Recursive case: keep dividing number by base.
   return is_power_of(result, base)
 
