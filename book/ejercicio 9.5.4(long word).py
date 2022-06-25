accc6ef2245f5a4daa06e397490ef6c63d966ca6
oxisakre <leoalvarg@gmail.com>
2022-06-25 18:48:24 +0200

    commit

--- book/ejercicio 9.5.4(long word).py
+++ book/ejercicio 9.5.4(long word).py
@@ -3,6 +3,8 @@ def jisus():
     texto = input('escribi algo: ')
     letra = input('letra:')
     max_palabra = ''
+    if len(letra) > 1:
+        raise IndexError("Elige una letra!")
     for i in texto.split():
         if letra in i:
             palabra = i
