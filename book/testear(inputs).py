596eb02c7be72e6ab1cb28778d355a1a33506be6
oxisakre <leoalvarg@gmail.com>
2022-06-25 18:11:04 +0200

    ejercicio

---
+++ book/testear(inputs).py
@@ -0,0 +1,18 @@
+def inputs():
+  number = int(input('x:'))
+  base = int(input('y:'))
+  return number,base
+
+def is_power_of(number, base):
+  # Base case: when number is smaller than base.
+  if number < base:
+    # If number is equal to 1, it's a power (base**0).
+    return number == 1
+  result = number//base
+  # Recursive case: keep dividing number by base.
+  return is_power_of(result, base)
+
+number, base = inputs()
+print(is_power_of(number, base))
+
+
