758e1a08cc37135a1be0bb58aee62a4d28319601
oxisakre <leoalvarg@gmail.com>
2022-06-27 20:15:40 +0200

    ejercicio

---
+++ book/ejercicio 17.5.7.py
@@ -0,0 +1,11 @@
+def combinaciones(letras, k, palabra=""):
+    if k == 0:
+        return
+    if len(palabra) == k:
+        print(palabra, end=' ')
+        return
+    for letra in letras:
+        combinaciones(letras, k, palabra + letra)
+
+
+combinaciones(('a', 'b', 'c'), 4)
