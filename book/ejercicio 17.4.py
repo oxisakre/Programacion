fa70744fac6fc2139ce78f15c860738287bf1928
oxisakre <leoalvarg@gmail.com>
2022-06-30 18:25:14 +0200

    ejercicio

--- book/ejercicio 17.4.py
+++ book/ejercicio 17.4.py
@@ -1,13 +1,15 @@
 def MaxValue(array):
     if len(array) == 0:
-        return ValueError()
+        raise ValueError()
     elif len(array) == 1:
         return array[0]
     else:
         j = array[0]
         y = MaxValue(array[1:])
-        return y if y >= j else j
-
+        if y >= j: 
+            return y
+        else:
+            return j
    
     
 
