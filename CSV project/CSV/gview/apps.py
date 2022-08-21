7eabf805462c07d5d07731bf2cb6615dac050cab
oxisakre <leoalvarg@gmail.com>
2022-08-21 16:49:09 +0200

    CSV

---
+++ CSV project/CSV/gview/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class GviewConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'gview'
---
+++ myfile.py
@@ -0,0 +1,4 @@
+def aver(x):
+    print(x % 3)
+
+aver(6)
