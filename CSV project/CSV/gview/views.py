9012f1f4c93f0ca6518b2b75e7c2feb377c2c7ed
oxisakre <leoalvarg@gmail.com>
2022-08-24 01:56:13 +0200

    person class

--- CSV project/CSV/gview/views.py
+++ CSV project/CSV/gview/views.py
@@ -1,16 +1,14 @@
-from gview.models import Person,Gender,Identification
+from gview.models import Person
 
 from django.views import View
 from django.shortcuts import render
 
 # Create your views here.
 
-def index(request):
-    return render(request, 'testing.html')
-
 class idListView(View):
-    def listview(self, request):
-        information = Identification.objects.all()
-        return render(request, 'testing.html', {'data': information})
-    
+    def get(self, request):
+        data = Person.objects.all()
+        return render(request, 'testing.html', {'data': data})
+
+
     
