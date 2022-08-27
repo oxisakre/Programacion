81a181ef99a91309aaced35dbf6b297fce6af4de
oxisakre <leoalvarg@gmail.com>
2022-08-27 16:34:41 +0200

    trying html

--- CSV project/CSV/gview/urls.py
+++ CSV project/CSV/gview/urls.py
@@ -1,8 +1,11 @@
+from re import template
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.idListView.as_view(), name='testing'),
+    path('', views.MainView.as_view(template_name='templates/main.html'), name='main'),
+    path('page1', views.MainView.as_view(template_name='templates/main.html'), name='page1'),
+    path('page2', views.MainView.as_view(template_name='templates/main.html'), name='page2'),
+    path('page3', views.MainView.as_view(template_name='templates/main.html'), name='page3'),
     path('list', views.idListView.as_view(), name='data'),
-
 ]
--- CSV project/CSV/gview/views.py
+++ CSV project/CSV/gview/views.py
@@ -1,9 +1,14 @@
+from re import template
 from gview.models import Person
 
 from django.views import View
 from django.shortcuts import render
 
 # Create your views here.
+class MainView(View):
+    template_name = None
+    def get(self, request):
+        return render(request, 'main.html')
 
 class idListView(View):
     def get(self, request):
