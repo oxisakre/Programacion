3ee57a6fdf271c950a529d6d0034d439b9595f02
oxisakre <leoalvarg@gmail.com>
2022-08-29 19:05:31 +0200

    barra

--- CSV project/CSV/gview/urls.py
+++ CSV project/CSV/gview/urls.py
@@ -4,8 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.MainView.as_view(template_name='templates/main.html'), name='main'),
-    path('page1', views.MainView.as_view(template_name='templates/main.html'), name='page1'),
-    path('page2', views.MainView.as_view(template_name='templates/main.html'), name='page2'),
-    path('page3', views.MainView.as_view(template_name='templates/main.html'), name='page3'),
-    path('list', views.idListView.as_view(), name='data'),
+    path('table', views.idListView.as_view(), name='Table'),
 ]
