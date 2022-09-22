5a300510c10cde8871072dd275b9b38a338f1bce
oxisakre <leoalvarg@gmail.com>
2022-09-22 19:17:53 +0200

    add root

--- CSV project/CSV/CSV/settings.py
+++ CSV project/CSV/CSV/settings.py
@@ -121,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 DATA_ROOT = os.path.join(BASE_DIR, 'data')
 DATA_URL = '/data/'
