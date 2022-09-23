b70ee0f5f18a6c0035fc1efa6a1e565d5942636c
oxisakre <leoalvarg@gmail.com>
2022-09-23 19:06:29 +0200

    change
    as

--- CSV project/CSV/gview/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class GviewConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'gview'
--- CSV project/CSV/gview/forms.py
+++
@@ -1,5 +0,0 @@
-from django import forms
-
-class UploadFileForm(forms.form):
-    title = forms.CharField(max_length=100)
-    file = forms.FileField 
